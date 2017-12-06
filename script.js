c0fbfe178bc759fbce39ca4604e50820e9a236e1
jonathanbell <bell.jo@husky.neu.edu>
2017-12-06 13:24:41 -0500

    added expandable sections

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ $(window).on("scroll touchmove", function () {
     $('#circle').toggleClass('shrink', $(document).scrollTop() > 0);
     $('#spacer').toggleClass('shrink', $(document).scrollTop() > 0);
 });
+
+function showSection(section, arrow) {
+  document.getElementById(section).classList.toggle('show');
+  document.getElementById(arrow).classList.toggle('rotate');
+}
